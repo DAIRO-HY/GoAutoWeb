247f59343a9a23c6d3b0f3a590b1e6b827fe66eb
zhoulq <1064348912@qq.com>
2025-01-07 19:40:33 +0900

    表单验证Email,NotEmpty,length,limit,半角英数字字符 完成

--- ReadFormUtil/ValidateBean.go
+++ ReadFormUtil/ValidateBean.go
@@ -17,11 +17,10 @@ type ValidateBean struct {
 
 // 生成表单验证部分代码
 func (mine *ValidateBean) MakeValidSource(field string) string {
-	msg := mine.Args["msg"]
 	formField := strings.ToLower(field[:1]) + field[1:]
 	source := ""
 	if mine.Name == "NOTEMPTY" { //非空验证
-		source += fmt.Sprintf("\t\tisNotEmpty(filedError, \"%s\", valid%s, \"%s\") // 非空验证\n", formField, field, msg)
+		source += fmt.Sprintf("\t\tisNotEmpty(filedError, \"%s\", valid%s) // 非空验证\n", formField, field)
 	} else if mine.Name == "LENGTH" { //长度验证代码
 		minlength := mine.Args["min"]
 		maxlength := mine.Args["max"]
@@ -35,7 +34,7 @@ func (mine *ValidateBean) MakeValidSource(field string) string {
 		} else {
 			maxlength = "intP(" + maxlength + ")"
 		}
-		source += fmt.Sprintf("\t\tisLength(filedError, \"%s\", valid%s, %s, %s, \"%s\")// 输入长度验证\n", formField, field, minlength, maxlength, msg)
+		source += fmt.Sprintf("\t\tisLength(filedError, \"%s\", valid%s, %s, %s)// 输入长度验证\n", formField, field, minlength, maxlength)
 	} else if mine.Name == "LIMIT" { //数值值区间验证
 		minValue := mine.Args["min"]
 		maxValue := mine.Args["max"]
@@ -49,9 +48,38 @@ func (mine *ValidateBean) MakeValidSource(field string) string {
 		} else {
 			maxValue = "floatP(" + maxValue + ")"
 		}
-		source += fmt.Sprintf("\t\tisLimit(filedError, \"%s\", valid%s, %s, %s, \"%s\")// 数值值区间验证\n", formField, field, minValue, maxValue, msg)
+		source += fmt.Sprintf("\t\tisLimit(filedError, \"%s\", valid%s, %s, %s)// 数值值区间验证\n", formField, field, minValue, maxValue)
+	} else if mine.Name == "DIGITS" { //数值验证
+		integer := mine.Args["integer"]
+		fraction := mine.Args["fraction"]
+		if integer == "" {
+			integer = "0"
+		}
+		if fraction == "" {
+			fraction = "0"
+		}
+		source += fmt.Sprintf("\t\tisDigits(filedError, \"%s\", valid%s, %s, %s)// 数值值区间验证\n", formField, field, integer, fraction)
+	} else if mine.Name == "HALF" { //半角验证
+
+		upper := mine.Args["upper"]
+		lower := mine.Args["lower"]
+		number := mine.Args["number"]
+		symbol := mine.Args["symbol"]
+		if upper != "false" {
+			upper = "true"
+		}
+		if lower != "false" {
+			lower = "true"
+		}
+		if number != "false" {
+			number = "true"
+		}
+		if symbol != "false" {
+			symbol = "true"
+		}
+		source += fmt.Sprintf("\t\tisHalf(filedError, \"%s\", valid%s, %s, %s, %s, %s)// 半角字符验证\n", formField, field, upper, lower, number, symbol)
 	} else if mine.Name == "EMAIL" {
-		source += fmt.Sprintf("\t\tisEmail(filedError, \"%s\", valid%s, \"%s\") // 邮箱格式验证\n", formField, field, msg)
+		source += fmt.Sprintf("\t\tisEmail(filedError, \"%s\", valid%s) // 邮箱格式验证\n", formField, field)
 	}
 	return source
 }
